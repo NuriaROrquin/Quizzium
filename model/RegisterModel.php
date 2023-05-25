9b289657aba87628cea66a99c0350fa454106231
Matias Ariel Coco <cocomatias6@gmail.com>
2023-05-24 23:50:36 -0300

    subida-de-usuario-a-la-db
    Aca hice la funcion de subir el usuario a la base de datos y la validacion de los datos ingresados (falta poner la foto y separar nombre y apellido en la vista)

--- model/RegisterModel.php
+++ model/RegisterModel.php
@@ -10,20 +10,62 @@ class RegisterModel
         $this->database = $database;
     }
 
+    private function insertUser($fields)
+    {
+        /*$sql = "INSERT INTO `cuenta` (`id_genero`, `mail`, `ciudad`, `pais`, `usuario`, `contrasenia`,
+                        `foto_perfil`, `fecha_nacimiento`, `nombre`, `apellido`) VALUES ('{$fields['gender']}' ,
+                        {$fields['mail']} , {$fields['city']} , {$fields['country']} , {$fields['username']} , {$fields['password']}
+                        , '' , {$fields['born_date']} , {$fields['name']} , {$fields['name']} );";*/
+
+        //$this->database->query($sql);
+        echo "aca tengo que redireccionar al login o algo que diga que la cuenta fue creada correctamente y que falta validar el correo enviado mail";
+    }
+
+    private function validatePassword($password, $verificated_password)
+    {
+        $validate = false;
+
+        if ($password == $verificated_password) {
+            $validate = true;
+        }
+
+        return $validate;
+    }
+
+    private function validateMail($mail)
+    {
+        $validate = false;
+
+        if (filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+            $validate = true;
+        }
+
+        return $validate;
+    }
+
     public function validate($fields)
     {
-        /*foreach ($fields as $field) {
-            if(!isset($field)){
 
+        foreach ($fields as $field) {
+            if (empty($field)) {
+                exit("Hay campos que faltan completar");
             }
-        }*/
+        }
 
-        $sql = "INSERT INTO `cuenta` (`id_genero`, `mail`, `ciudad`, `pais`, `usuario`, `contrasenia`, `foto_perfil`, `fecha_nacimiento`, `nombre`, `apellido`) VALUES (" . $fields['gender'] . "," . $fields['mail'] . "," . $fields['city'] . "," . $fields['country'] . "," . $fields['username'] . "," . $fields['password'] . ", '' ," . $fields['born_date'] . "," .  $fields['name'] . "," .  $fields['name'] .");";
+        if($this->validatePassword($fields['password'], $fields['verificated_password'])){
 
-        $this->database->query($sql);
+            if($this->validateMail($fields['mail'])){
+                $this->insertUser($fields);
+            }
 
+            else {
+                exit("El correo electrónico no es válido");
+            }
+        }
 
-
-        echo "bien!";
+        else {
+            exit("Las contraseñas no son iguales");
+        }
     }
 }
+
