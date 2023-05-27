d00a0c9f96015b81f7267aa1be94d6c28aee7310
NuriaROrquin <67746320+NuriaROrquin@users.noreply.github.com>
2023-05-27 20:01:09 -0300

    add qr profile

--- controller/ProfileController.php
+++ controller/ProfileController.php
@@ -16,10 +16,10 @@ class ProfileController
         $id_cuenta= $_GET['id_cuenta'];
 
         //soy owner del user: si el id_cuenta es el mismo id que tengo en el session llenar owner
-        //$data["owner"] = $this->profileModel->getProfile($id_cuenta);
+        $data["owner"] = $this->profileModel->getProfile($id_cuenta);
 
         //no soy owner del user: si el id_cuenta no es el mismo id que tengo en el session llenar public
-        $data["public"] = $this->profileModel->getProfile($id_cuenta);
+        //$data["public"] = $this->profileModel->getProfile($id_cuenta);
 
         $this->renderer->render('profile', $data);
     }
