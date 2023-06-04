a516e6d962b5e89482ba163684d115d40069a094
vgambaro <victoriagambaro@gmail.com>
2023-06-04 09:56:26 -0300

    arreglando partida

--- controller/PlayController.php
+++ controller/PlayController.php
@@ -31,24 +31,63 @@ class PlayController
             exit();
         } else {
             $answer = $_POST['option'] ?? false;
-
+            unset($_POST['option']);
             if ($answer) {
-                $isCorrect = $this->playModel->verificateAnswer($answer);
-
+                $isCorrect = $this->verificateAnswer($answer);
 
                 if ($isCorrect) {
                     $question = $this->playModel->play();
-
+                    $question['puntuacion'] = $_SESSION['puntuacion'];
+                    $question['categoryColor'] = $this->playModel->showCategory($question['categoria']);
+                    $id_cuenta = $_SESSION['userID']['id_cuenta'];
+                    $userinfo = $this->playModel->getUserData($id_cuenta);
+                    $question['foto_perfil'] = $userinfo['foto_perfil'];
+                    $question['usuario'] = $userinfo['usuario'];
                     $this->renderer->render('play', $question ?? "");
-                } else {
+                }
+                else {
                     $puntuacion = $_SESSION['puntuacion'];
                     unset($_SESSION['puntuacion']);
                     var_dump($puntuacion);
                 }
-            } else {
+            }
+            else {
+                unset($_SESSION['puntuacion']);
                 $question = $this->playModel->play();
+                $question['puntuacion'] = 0;
+                $question['categoryColor'] = $this->playModel->showCategory($question['categoria']);
+                $id_cuenta = $_SESSION['userID']['id_cuenta'];
+                $userinfo = $this->playModel->getUserData($id_cuenta);
+                $question['foto_perfil'] = $userinfo['foto_perfil'];
+                $question['usuario'] = $userinfo['usuario'];
                 $this->renderer->render('play', $question ?? "");
             }
         }
     }
+
+    private function verificateAnswer($answer)
+    {
+
+        if (!isset($_SESSION['puntuacion'])) {
+            $_SESSION['puntuacion'] = 0;
+        }
+
+        $isCorrect = false;
+        if ($answer == $_SESSION['opcionCorrecta']) {
+            unset($_SESSION['opcionCorrecta']);
+            $isCorrect = true;
+
+            $_SESSION['puntuacion'] = $_SESSION['puntuacion'] + 1;
+
+        }
+        return $isCorrect;
+    }
+
+    private function showScore()
+    {
+
+
+        return $isCorrect;
+    }
+
 }
--- model/PlayModel.php
+++ model/PlayModel.php
@@ -18,7 +18,7 @@ class PlayModel
 
     private function bringQuestions($idString)
     {
-        $sql = "SELECT p.`pregunta`, o.`opcion`, o.`es_correcta`, p.`id_pregunta` FROM `pregunta` p JOIN `opcion` o
+        $sql = "SELECT p.`pregunta`, o.`opcion`, o.`es_correcta`, p.`id_pregunta`, p.`id_categoria` FROM `pregunta` p JOIN `opcion` o
          ON p.`id_pregunta` = o.`id_pregunta` WHERE p.`id_pregunta` = " . $idString . ";";
 
         $questions = $this->database->query($sql);
@@ -31,6 +31,7 @@ class PlayModel
         $question['opcion2'] = $fila[1]['opcion'];
         $question['opcion3'] = $fila[2]['opcion'];
         $question['opcion4'] = $fila[3]['opcion'];
+        $question['categoria'] = $fila[0]['id_categoria'];
 
         foreach ($fila as $clave => $respuesta) {
             if ($respuesta['es_correcta'] == 1) {
@@ -51,23 +52,38 @@ class PlayModel
         return $question;
     }
 
-    public function verificateAnswer($answer)
+    public function showCategory($category)
     {
+        switch($category){
+            case '1':
+                $color ='#008639';
+                break;
+            case '2':
+                $color ='#BEA821';
+                break;
+            case '3':
+                $color ='#DC0000';
+                break;
+            case '4':
+                $color ='#0176D2';
+                break;
+            case '5':
+                $color ='#FF69B4';
+                break;
+            case '6':
+                $color ='#FF9400';
+                break;
 
-        if (!isset($_SESSION['puntuacion'])) {
-            $_SESSION['puntuacion'] = 0;
         }
-
-        $isCorrect = false;
-        if ($answer == $_SESSION['opcionCorrecta']) {
-            unset($_SESSION['opcionCorrecta']);
-            $isCorrect = true;
-
-            $_SESSION['puntuacion'] = $_SESSION['puntuacion'] + 1;
-
-        }
-        return $isCorrect;
+        return 'background-color:'.$color.';';
     }
+
+    public function getUserData($id_cuenta)
+    {
+       return $this->database->querySelectAssoc("SELECT foto_perfil, usuario FROM cuenta WHERE id_cuenta = '$id_cuenta'");
+
+    }
+
 }
 
 
