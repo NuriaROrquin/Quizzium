66462e2a73ae40d8a4baa463b8b4c56215b10a22
NuriaROrquin <67746320+NuriaROrquin@users.noreply.github.com>
2023-06-10 04:37:10 -0300

    add list ranking

--- model/RankingModel.php
+++ model/RankingModel.php
@@ -11,19 +11,24 @@ class RankingModel
 
     public function getRanking($id_cuenta)
     {
-        $results = $this->database->querySelectAll("SELECT j.id_cuenta, MAX(j.puntaje) AS puntaje_maximo, c.id_cuenta,c.usuario, c.foto_perfil, c.nombre, c.apellido FROM juego j JOIN cuenta c ON j.id_cuenta = c.id_cuenta GROUP BY j.id_cuenta;");
+        $statement = $this->database->query("SELECT j.id_cuenta, MAX(j.puntaje) AS puntaje_maximo, c.id_cuenta,c.usuario, c.foto_perfil, c.nombre, c.apellido FROM juego j JOIN cuenta c ON j.id_cuenta = c.id_cuenta GROUP BY j.id_cuenta;");
 
-        $recordsWithIndex = array_map(function ($indexResult, $result) use ($id_cuenta) {
-            foreach ($result as $id){
-                if($id_cuenta == $id){
-                    return ['index' => $indexResult+1, 'style' => 'background-color:#FFA500; color: #fff;','result' => $id];
-                }else{
-                    return ['index' => $indexResult+1, 'result' => $id];
-                }
+        $resultados = array();
+
+        $index = 1;
+
+        while ($fila = $statement->fetch_assoc()) {
+            $fila['index'] = $index;
+
+            if($id_cuenta == $fila['id_cuenta']){
+                $fila['style'] = 'background-color:#FFA500; color: #fff;';
             }
-        }, array_keys($results), $results);
 
-        $data["rankingList"] = $recordsWithIndex;
+            $resultados[] = $fila;
+            $index++;
+        }
+
+        $data["rankingList"] = $resultados;
 
         return $data;
     }
