47d580f03a936e2f4a7d3ff7838ff0830776ca93
Matias Ariel Coco <cocomatias6@gmail.com>
2023-06-24 15:59:11 -0300

    update querys faltantes
    falta hacer que el model entregue el mensaje o crear un helper que ordene esto

--- model/StatisticsModel.php
+++ model/StatisticsModel.php
@@ -123,4 +123,124 @@ class StatisticsModel
         return $result;
     }
 
+    public function getPlayers()
+    {
+
+        $sql = "
+        SELECT COUNT(id_cuenta) AS cantidad_de_jugadores
+        FROM cuenta ;";
+
+        $result = $this->database->querySelectAssoc($sql);
+
+        return $result['cantidad_de_jugadores'];
+    }
+
+    public function getNumberOfGames()
+    {
+
+        $sql = "
+        SELECT COUNT(id_juego) AS cantidad_de_partidas
+        FROM juego ;";
+
+        $result = $this->database->querySelectAssoc($sql);
+
+        return $result['cantidad_de_partidas'];
+    }
+
+    public function getNumberOfActiveQuestions()
+    {
+
+        $sql = "
+        SELECT COUNT(id_pregunta) AS cantidad_de_preguntas_activas
+        FROM pregunta 
+        WHERE esta_activa = 1;";
+
+        $result = $this->database->querySelectAssoc($sql);
+
+        return $result['cantidad_de_preguntas_activas'];
+    }
+
+    public function getNumberOfQuestionsViwed()
+    {
+
+        $sql = "
+        SELECT COUNT(id_sugerencia) AS cantidad_de_preguntas_vistas
+        FROM sugerencia
+        WHERE fue_vista=1;";
+
+        $result = $this->database->querySelectAssoc($sql);
+
+        return $result['cantidad_de_preguntas_vistas'];
+    }
+
+    public function getNumberOfUnseenQuestions()
+    {
+
+        $sql = "
+        SELECT COUNT(id_sugerencia) AS cantidad_de_preguntas_sin_ver
+        FROM sugerencia
+        WHERE fue_vista=0;";
+
+        $result = $this->database->querySelectAssoc($sql);
+
+        return $result['cantidad_de_preguntas_sin_ver'];
+    }
+
+    public function getPercentageOfEffectivenessPerUser()
+    {
+
+        $sql = "
+        SELECT dificultad
+        FROM cuenta;";
+
+        $result = $this->database->querySelectAssoc($sql);
+
+        $result = $this->database->querySelectAssoc($sql);
+
+        $users = array();
+
+        foreach ($result as $row) {
+
+            $user = new stdClass();
+
+            $user->usuario = $row['id_cuenta'];
+
+            $user->usuario = $row['usuario'];
+
+            $user->dificultad = $row['dificultad'];
+
+            $users[] = $user;
+
+        }
+
+        return $users;
+    }
+
+
+    public function getNewUsers()
+    {
+        $fechaActual = date("Y-m-d");
+
+        $fechaHaceTresDias = date("Y-m-d", strtotime("-7 days"));
+
+        $sql = "SELECT * FROM cuenta WHERE fecha_creacion BETWEEN '$fechaHaceTresDias' AND '$fechaActual'";
+
+        $result = $this->database->querySelectAssoc($sql);
+
+        $message="";
+
+        if ($result->num_rows > 0) {
+
+            while ($row = $result->fetch_assoc()) {
+
+                $message = $message .
+                    "<br><p>ID de cuenta: " . $row["id_cuenta"] . "<p><br>" .
+                    "<p>Fecha de creación: " . $row["fecha_creacion"] . "</p><br>";
+            }
+        } else {
+            $message = "No se encontraron cuentas nuevas.";
+        }
+
+        return $message;
+    }
 }
