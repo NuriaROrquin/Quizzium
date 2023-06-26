f1891bfec86aa5304f56c86176825f138c781d93
NuriaROrquin <67746320+NuriaROrquin@users.noreply.github.com>
2023-06-25 23:08:14 -0300

    fix pdf

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -121,20 +121,6 @@ class AdminController
     {
         $data = json_decode($_POST['data'], true);
 
-        $dateFrom = $data['dateFrom'];
-        $dateTo = $data['dateTo'];
-        $totalPlayers = $data['total_players'];
-        $totalGames = $data['total_games'];
-        $totalQuestionsActive = $data['total_questions_active'];
-        $totalSuggestions = $data['total_suggestions'];
-        $totalViewedSuggestions = $data['total_viwed_suggestions'];
-        $percentageEffectiveForPlayer = $data['percentage_effective_for_player'];
-        $totalNewUsers = $data['total_new_users'];
-        $byCountry = $data['by_country'];
-        $image = $data['by_country'];
-        $image = $data['by_age'];
-        $image = $data['by_gender'];
-
         $this->statisticsModel->getPDF($data);
     }
 }
